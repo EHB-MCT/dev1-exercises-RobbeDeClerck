da4adc9ee740574010c33f66b66fb384e7ce1aae
Robbe De Clerck <robbe.de.clerck@student.ehb.be>
2022-10-05 15:16:53 +0200

    Naam

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -10,11 +10,100 @@ drawName();
 function drawName(){
 
     context.lineWidth = 5;
-    context.strokeStyle = "red";
-    //vorm start (tekening)
-    context.beginPath();
-    context.rect(0, 0, 400, 500);
-    context.stroke();
-    context.fill();
+    context.strokeStyle = "black";
 
+    //R
+    context.beginPath();
+    context.moveTo(20, 20);
+    context.lineTo(20, 100);
+
+    context.moveTo(60, 20);
+    context.lineTo(20, 20);
+
+    context.moveTo(60, 20);
+    context.lineTo(60, 40);
+    context.lineTo(20, 40);
+
+    context.moveTo(60, 100);
+    context.lineTo(20, 40);
+
+    context.stroke();
+
+    //O
+    context.beginPath();
+    context.moveTo(100, 20);
+    context.lineTo(150, 20);
+    context.lineTo(150, 100);
+    context.lineTo(100, 100);
+    context.lineTo(100, 20);
+
+    context.stroke();
+
+    //B
+    context.beginPath();
+    context.moveTo(180, 20);
+    context.lineTo(220, 20);
+
+    context.moveTo(180, 65);
+    context.lineTo(220, 65);
+
+    context.moveTo(180, 75);
+    context.lineTo(240, 75);
+
+    context.moveTo(220, 20);
+    context.lineTo(220, 65);
+
+    context.moveTo(240, 75);
+    context.lineTo(240, 100);
+
+    context.moveTo(180, 100);
+    context.lineTo(180, 20);
+
+    context.moveTo(180, 100);
+    context.lineTo(240, 100);
+
+    context.stroke();
+
+    //B
+
+    context.beginPath();
+    context.moveTo(280, 20);
+    context.lineTo(320, 20);
+
+    context.moveTo(280, 65);
+    context.lineTo(320, 65);
+
+    context.moveTo(280, 75);
+    context.lineTo(340, 75);
+
+    context.moveTo(320, 20);
+    context.lineTo(320, 65);
+
+    context.moveTo(340, 75);
+    context.lineTo(340, 100);
+
+    context.moveTo(280, 100);
+    context.lineTo(280, 20);
+
+    context.moveTo(280, 100);
+    context.lineTo(340, 100);
+
+    context.stroke();
+
+    //E
+
+    context.beginPath();
+    context.moveTo(380, 20);
+    context.lineTo(380, 100);
+
+    context.moveTo(380, 20);
+    context.lineTo(420, 20);
+
+    context.moveTo(380, 60);
+    context.lineTo(420, 60);
+
+    context.moveTo(380, 100);
+    context.lineTo(420, 100);
+
+    context.stroke();
 }
