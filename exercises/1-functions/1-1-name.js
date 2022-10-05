8b96cc092cddbc059c8a02594f2b8d44206cf5b4
Robbe De Clerck <robbe.de.clerck@student.ehb.be>
2022-10-05 14:39:42 +0200

    Exercises 2

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawName();
+
+function drawName(){
+
+    context.lineWidth = 5;
+    context.strokeStyle = "red";
+    //vorm start (tekening)
+    context.beginPath();
+    context.rect(0, 0, 400, 500);
+    context.stroke();
+    context.fill();
+
+}
