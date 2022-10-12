af55669a2ca1a7b0525a47e49e95ba5a09bf8451
Robbe De Clerck <robbe.de.clerck@student.ehb.be>
2022-10-12 15:05:02 +0200

    Random colours

---
+++ exercises/1-functions/2-2.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+let margin = 50;
+let sizeSquare = 600;
+
+let colorRandom;
+let colorRandomTwo;
+let colorRandomThree;
+let colorSum;
+
+drawRandomColors(margin, sizeSquare, colorSum);
+drawRandomColors(margin, sizeSquare, colorSum);
+drawRandomColors(margin, sizeSquare, colorSum);
+drawRandomColors(margin, sizeSquare, colorSum);
+drawRandomColors(margin, sizeSquare, colorSum);
+drawRandomColors(margin, sizeSquare, colorSum);
+drawRandomColors(margin, sizeSquare, colorSum);
+
+function drawRandomColors(startpos, size, color){
+
+    let colorRandom = Math.floor(Math.random() * 10);
+    let colorRandomTwo = Math.floor(Math.random() * 10);
+    let colorRandomThree = Math.floor(Math.random() * 10);
+    let colorSum = "#" + colorRandom + colorRandom + colorRandomTwo + colorRandomTwo + colorRandomThree + colorRandomThree;
+    
+    context.beginPath();
+    context.rect(startpos, startpos, size, size);
+    context.fillStyle = color;
+    context.fill();
+
+    console.log(context.fillStyle = colorSum);
+    margin += 50;
+    sizeSquare -= 100;
+
+}
